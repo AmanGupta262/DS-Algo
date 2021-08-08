4b5a7085981aa1f0ffa7993b28ec691e363b78ec
Aman Gupta <amang5840@gmail.com>
2021-08-08 17:29:17 +0530

    alpha pattern

---
+++ Patterns1/alphaPattern.java
@@ -0,0 +1,27 @@
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 3
+//  A
+//  BB
+//  CCC
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        char ch = 'A';
+        
+        int i = 1;
+        while( i <= n){
+            int j = 1;
+            while(j <= i){
+                System.out.print(ch);
+                j += 1;
+            }
+            System.out.println();
+            i += 1;
+            ch += 1;
+        }
+	}
+}
