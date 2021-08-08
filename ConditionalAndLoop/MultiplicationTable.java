1086ff8e030476d077f34a0f09c3b0e17d88b28b
Aman Gupta <amang5840@gmail.com>
2021-08-08 11:32:16 +0530

    multiplication table

---
+++ ConditionalAndLoop/MultiplicationTable.java
@@ -0,0 +1,14 @@
+// Write a program to print multiplication table of n
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i=1;
+        while(i <= 10){
+            System.out.println(n * i);
+            i += 1;
+        }
+	}
+}
