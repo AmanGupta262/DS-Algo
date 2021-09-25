870e1d3161ff4e0c8102664b9d7c8839030a6696
Aman Gupta <amang5840@gmail.com>
2021-09-25 11:15:03 +0530

    Parallelogram pattern

---
+++ Patterns2/parallelogramPattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int i = 1;
+        while(i <= n){
+            printPattern(1, i-1, ' ');
+            printPattern(1, n, '*');
+            i += 1;
+            System.out.println();
+        }
+
+	}
+    public static void printPattern(int start, int end, char ch){
+    	while(start <= end){
+            System.out.print(ch);
+            start += 1;
+        }
+    }
+}
