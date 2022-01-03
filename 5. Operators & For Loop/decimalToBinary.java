6a40e51fbb4ee578a34d9d175864d0ce9051a222
Aman Gupta <amang5840@gmail.com>
2022-01-03 23:06:00 +0530

    Decimal to binary

--- 5. Operators & For Loop/decimalToBinary.java
+++ 5. Operators & For Loop/decimalToBinary.java
@@ -1,19 +1,15 @@
-/**
- * 
- */
 import java.util.Scanner;
-public class decimalToBinary {
-    public static void main(String[] args) {
-		// Write your code hereScanner s= new Scanner (System.in);
-        
-        Scanner s = new Scanner(System.in);
+
+class decimalToBinary {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		long decimal = 0, pv=1;
-		while( n >0) {
-			int rem = n%2;
-			decimal = decimal+ rem * pv;
-			n/=2;
-			pv*=10;
+		long decimal = 0, p = 0;
+		while (n > 0) {
+			int bit = n & 1;
+			decimal = (long) (bit * Math.pow(10, p) + decimal);
+			n >>= 1;
+			p += 1;
 		}
 		System.out.print(decimal);
 	}
